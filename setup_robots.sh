b1902888105ef73147e0c9b26edfc2176e0fd4e6
Pedro Barros <85529147+jandsonbarros13@users.noreply.github.com>
2025-12-30 01:23:47 +0000

    Update setup_robots.sh

--- setup_robots.sh
+++ setup_robots.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-
 PROJECT_DIR="/usr/bin/ultron"
-
 cd $PROJECT_DIR
 
+# Instala dependências do Node
 if [ ! -d "$PROJECT_DIR/node_modules" ]; then
     npm install @google/generative-ai dotenv express cors pg axios --save
 fi
 
+# Instala Ollama se não existir
 if ! command -v ollama &> /dev/null; then
     curl -fsSL https://ollama.com/install.sh | sh
 fi
 
-if ! pgrep -x "ollama" > /dev/null; then
-    ollama serve > /dev/null 2>&1 &
-    sleep 10
-fi
+ollama serve > /dev/null 2>&1 &
+sleep 5
 
 ollama pull llama3
 
